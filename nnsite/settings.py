28a17383c90e5e8ff3a1121b0bcfc39fcb364f39
youremail@yourdomain.com <naufaln176@gmail.com>
2022-12-17 00:22:00 +0700

    update display

--- nnsite/settings.py
+++ nnsite/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_URL = 'theme/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'theme\static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
