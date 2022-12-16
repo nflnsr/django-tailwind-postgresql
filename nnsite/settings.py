d4a276d27f454907a26f1d5b1008e3199e72a089
youremail@yourdomain.com <naufaln176@gmail.com>
2022-12-17 01:06:30 +0700

    update display

--- nnsite/settings.py
+++ nnsite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'django_browser_reload',
+    
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -138,8 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'theme/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'theme/static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
